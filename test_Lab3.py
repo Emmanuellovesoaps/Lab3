c380587d86e17311580260d5f650bcad3672a17a
Emmanuel <emmanuelongsh25.24@ichat.sp.edu.sg>
2025-05-10 17:38:24 +0800

    Tested Lab3 code

--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,25 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_n_equal_0():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == 0)
+def test_bubble_sort_n_more_than_or_equal_10():
+    result = []
+    input_arr = [1,2,3,4,5,6,7,8,9,10]  
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 1)
+def test_bubble_sort_list_has_non_integers():
+    result = []
+    input_arr = [1, 2,"a", 4]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 2)
